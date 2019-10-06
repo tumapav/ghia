6dbfe24d36f1d462033b5265da2ed697095de3c5
Pavel Tuma <tumapav3@fit.cvut.cz>
2019-10-06 19:52:08 +0200

    Reposlug validation

--- ghia.py
+++ ghia.py
@@ -1,8 +1,28 @@
 import click
+import re
+
+
+def validate_reposlug(ctx, param, value):
+    _VALIDATION_ERR = "not in owner/repository format"
+
+    # Check github name/repo format
+    res = re.match('^[A-Za-z0-9\.-_]+/[A-Za-z0-9\.-_]+$', value)
+    if res is None:
+        raise click.BadParameter(_VALIDATION_ERR)
+
+    parts = value.split("/")
+
+    # Dot alone is a reserved name at github
+    if parts[0] == "." or parts[1] == ".":
+        raise click.BadParameter(_VALIDATION_ERR)
+
+    return True
 
 
 @click.command()
-@click.argument('reposlug')
+@click.argument('reposlug',
+                callback=validate_reposlug,
+                type=click.STRING)
 @click.option('-s', '--strategy',
               help='How to handle assignment collisions.',
               type=click.Choice(['append', 'set', 'change']),
@@ -20,10 +40,10 @@ import click
               help='File with assignment rules configuration.',
               required=True,
               type=click.File('r'))
-def hello(reposlug, strategy, dry_run, config_auth, config_rules):
+def ghia(reposlug, strategy, dry_run, config_auth, config_rules):
     """CLI tool for automatic issue assigning of GitHub issues"""
     pass
 
 
 if __name__ == '__main__':
-    hello()
+    ghia()
